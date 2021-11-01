2075cea62bc4e0f38ec1129c6d8219cdff8aac90
ArthurHtbk <arthur.heurtebise@gmail.com>
2021-11-01 18:04:54 +0100

    responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,26 +13,28 @@ const Header = ({ userToken, setUser }) => {
           <input type="text" placeholder="Recherche des articles" />
           <FontAwesomeIcon icon="search" className="searchIcon" />
         </form>
-        {userToken ? (
-          <Link to="/">
-            <button onClick={() => setUser(null)} className="logout">
-              Se déconnecter
-            </button>
-          </Link>
-        ) : (
-          <div className="login-buttons">
-            <Link to="/signup">
-              <button className="signup">S'inscrire</button>
-            </Link>{" "}
-            <Link to="/login">
-              <button className="login">Se connecter</button>
+        <div className="header-buttons">
+          {userToken ? (
+            <Link to="/">
+              <button onClick={() => setUser(null)} className="logout">
+                Se déconnecter
+              </button>
+            </Link>
+          ) : (
+            <div className="login-buttons">
+              <Link to="/signup" className="signup-link">
+                <button className="signup">S'inscrire</button>
+              </Link>{" "}
+              <Link to="/login">
+                <button className="login">Se connecter</button>
+              </Link>
+            </div>
+          )}
+          <div className="sell-button">
+            <Link to="/publish">
+              <button className="sell">Vends tes articles</button>
             </Link>
           </div>
-        )}
-        <div className="sell-button">
-          <Link to="/publish">
-            <button className="sell">Vends tes articles</button>
-          </Link>
         </div>
       </div>
     </header>
